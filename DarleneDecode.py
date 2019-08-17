c57b56528f73d64282050d628846ea7872485737
mazerlodge <mazerlodge@hotmail.com>
2019-08-16 22:02:30 -0500

    See POA file 08/16 entry for change list.

--- DarleneDecode.py
+++ DarleneDecode.py
@@ -14,17 +14,26 @@ DATA_FILE__WIN = ".\\darlene_code.txt"
 # Setup variables
 osType = "NOT_SET"
 dataPath = "NOT_SET"
+bDataFileSet = False
+wordIndex = -1
+charCount = -1
+skipChar = "!"
 
 def showUsage():
-    print("Usage: python DarleneDecode.py -os {mac | win} \n"
-    		+ "\t -os {mac | win} - Operating system selection (used to set dictionary path).\n")
+    print("Usage: python DarleneDecode.py [-os {mac | win} | -file datafile.txt] "
+			+ "-wordindex n -charcount c [-skipchar g]\n"
+    		+ "\t -os {mac | win} - Operating system selection (used to set dictionary path).\n"
+    		+ "\t -file filename.txt - (optional) data file to process.\n"
+    		+ "\t -wordindex n - process every n words (e.g. n=5, process every fifth word).\n"    		
+			+ "\t -charcount c - retrieve c chars from each word processed.\n"
+			+ "\t -skipchar g - (optional) if a target word starts with g, skip that letter when processing.\n")
     exit()
 
 def parseArgs():
 	# Parse the arguments looking for required parameters.
 	# Return false if any tests fail.
 	
-	global osType
+	global osType, dataPath, bDataFileSet, wordIndex, charCount, skipChar
 	
 	subtestResults = []
 	rval = True
@@ -32,13 +41,39 @@ def parseArgs():
 	# Instantiate the ArgParser
 	ap = ArgParser(sys.argv)
 
-	# check the OS type
+	# Check for wordIndex
 	rv = False
-	if (ap.isArgWithValue("-os", "mac") or ap.isArgWithValue("-os", "win")):
-		osType = ap.getArgValue("-os")
+	if (ap.isInArgs("-wordindex", True)):
+		wordIndex = int(ap.getArgValue("-wordindex"))
 		rv = True
 	subtestResults.append(rv)
+
+	# Check for characterCount
+	rv = False
+	if (ap.isInArgs("-charcount", True)):
+		charCount = int(ap.getArgValue("-charcount"))
+		rv = True
+	subtestResults.append(rv)
+
+	# Check for data file being specified.
+	if (ap.isInArgs("-file", True)):
+		dataPath = ap.getArgValue("-file")
+		bDataFileSet = True
+		rv = True
+	subtestResults.append(rv)
+
+	if (not bDataFileSet):
+		# if no data file specified, check for OS type
+		rv = False
+		if (ap.isArgWithValue("-os", "mac") or ap.isArgWithValue("-os", "win")):
+			osType = ap.getArgValue("-os")
+			rv = True
+		subtestResults.append(rv)
 	
+	# Check for optional skipChar
+	if (ap.isInArgs("-skipchar", True)):
+		skipChar = ap.getArgValue("-skipchar")
+
 	# Determine if all subtests passed
 	for idx in range(len(subtestResults)):
 		rval = rval and subtestResults[idx]
@@ -47,11 +82,14 @@ def parseArgs():
 	
 def getDarleneData():
 
-	# set data path based on OS
-	if (osType == "mac"):
-		dataPath = DATA_FILE__MAC 
-	else:
-		dataPath = DATA_FILE__WIN
+	global dataPath
+
+	if (not bDataFileSet):
+		# set data path based on OS
+		if (osType == "mac"):
+			dataPath = DATA_FILE__MAC 
+		else:
+			dataPath = DATA_FILE__WIN
 
 	# Read the data
 	file = open(dataPath, "r")
@@ -62,17 +100,36 @@ def getDarleneData():
 	
 def doDarleneDecode():
 
+	# Determine if a skip char was specified.
+	bSkipEnabled = False
+	if (skipChar != "!"):
+		bSkipEnabled = True
+
 	# Load the data into a lines string
 	lines = getDarleneData()
+	words = []
 
-	# split words
+	print (len(lines))
+
+	# split words, handling multi-line input and stripping excess whitespace
 	for line in lines:
-		words = line.split(' ')
+		if (line[0] != "#"):
+			rawWords = line.split(' ')
 		
-	targetwordindex = 5 
+			for aword in rawWords:
+				if (len(aword.strip(' \n\r\t')) > 0):
+					words.append(aword)
+	print(words)
+		
+	# zero based array, so reduce first wordIndex by 1.
+	targetwordindex = wordIndex-1 
 	while (targetwordindex < len(words)):
-		print(words[targetwordindex][0:2], end="")
-		targetwordindex += 6
+		if (bSkipEnabled and (words[targetwordindex][0] == skipChar)):
+			# skip the first char in the word 
+			print(words[targetwordindex][1:charCount+1], end="")
+		else:
+			print(words[targetwordindex][0:charCount], end="")
+		targetwordindex += wordIndex
 			
 	print()
 
@@ -81,5 +138,6 @@ def doDarleneDecode():
 # Validate startup parameters
 if (not parseArgs()):
 	showUsage()
+	exit()
 
 doDarleneDecode()
